a76635440e6f513001aff64cd4baddc7f8edcd76
Kasshi_K <kasshi.4259.b.b@gmail.com>
2021-08-18 16:34:26 +0900

    step2: Add reading timeout

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ extern crate termios;
 
 use std::io::{self, Read};
 use std::os::unix::io::{AsRawFd, RawFd};
-use termios::*;
 
 struct InputRawMode {
     fd: RawFd,
-    orig: Termios,
+    orig: termios::Termios,
 }
 
 impl InputRawMode {
     fn new(stdin: &io::Stdin) -> io::Result<InputRawMode> {
+        use termios::*;
+
         let fd = stdin.as_raw_fd();
         let mut termios = Termios::from_fd(fd)?;
         let orig = termios.clone();
@@ -19,8 +20,8 @@ impl InputRawMode {
         termios.c_oflag &= !(OPOST);
         termios.c_cflag |= CS8;
         termios.c_iflag &= !(ECHO | ICANON | IEXTEN | ISIG);
-        termios.c_cc[VMIN] = 1;
-        termios.c_cc[VTIME] = 0;
+        termios.c_cc[VMIN] = 0;
+        termios.c_cc[VTIME] = 1;
         tcsetattr(fd, TCSAFLUSH, &mut termios)?;
 
         Ok(InputRawMode { fd, orig })
@@ -28,16 +29,25 @@ impl InputRawMode {
 }
 impl Drop for InputRawMode {
     fn drop(&mut self) {
-        tcsetattr(self.fd, TCSAFLUSH, &mut self.orig).unwrap();
+        termios::tcsetattr(self.fd, termios::TCSAFLUSH, &mut self.orig).unwrap();
     }
 }
 
 fn main() -> io::Result<()> {
-    let stdin = io::stdin();
+    let mut stdin = io::stdin();
     let _raw = InputRawMode::new(&stdin)?;
-    for b in stdin.bytes() {
-        let c = b? as char;
-        println!("c:{}", c);
+    let mut one_byte: [u8; 1] = [0];
+
+    loop {
+        let size = stdin.read(&mut one_byte)?;
+        let c = if size > 0 { one_byte[0] as char } else { '\0' };
+
+        if c.is_control() {
+            print!("{}\r\n", c);
+        } else {
+            print!("char: {}\r\n", c);
+        }
+
         if c == 'q' {
             break;
         }
